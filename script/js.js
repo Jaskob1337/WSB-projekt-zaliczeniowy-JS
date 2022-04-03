82c6c9bfeb36728139b77e5316e07298de611051
Janek <you@example.comjaskobix@gmail.com>
2022-04-03 19:29:17 +0200

    first commit

---
+++ script/js.js
@@ -0,0 +1,21 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const allNavItems = document.querySelectorAll(".nav-link");
+	const navList = document.querySelector(".navbar-collapse");
+
+	function addShadow() {
+		if (window.scrollY >= 1) {
+			nav.classList.add("shadow-bg");
+		} else {
+			nav.classList.remove("shadow-bg");
+		}
+	}  
+
+allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+// pętla usuwająca automatycznie dodającą się klasę show 
+
+	window.addEventListener("scroll", addShadow);
+});
