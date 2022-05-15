e399887023c5a363f9b1c31bd2efa178cdc7adcb
Janek <you@example.comjaskobix@gmail.com>
2022-05-15 20:56:52 +0200

    cookies check

--- script/cookies.js
+++ script/cookies.js
@@ -1,35 +1,24 @@
-// import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.mjs'
-// const loadModal = function () {
-//   $('#CarModal').modal('show')
-//   Cookies.set('isMyModalActive', true)
-//   console.log('check111')
-// }
-// const exitModal = function () {
-//   $('#CarModal').modal('hide')
-//   Cookies.remove('isMyModalActive')
-// }
-// if (Cookies.get('isMyModalActive')) {
-//   loadModal()
-// }
-// console.log(Cookies)
-
 
 import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.mjs'
 const loadModal = function () {
-    $('#CarModal').modal('show');
-    Cookies.set('isMyModalActive', true);
-};
+  $('#CarModal').modal('show')
+  Cookies.set('isMyModalActive', true)
+}
 const exitModal = function () {
-    $('#CarModal').modal('hide');
-    Cookies.remove('isMyModalActive');
-};
+  $('#CarModal').modal('hide')
+  Cookies.remove('isMyModalActive')
+}
 const button = document.querySelector('[data-bs-toggle="modal"]')
 button.onclick = function () {
-    console.log('here')
-    loadModal()
+  loadModal()
 };
-window.addEventListener('load', function() {
-    if (Cookies.get('isMyModalActive')) {
-        loadModal();
-    }
+const button2 = document.getElementById('buttonexit')
+button2.onclick = function () {
+  exitModal()
+};
+
+window.addEventListener('load', function () {
+  if (Cookies.get('isMyModalActive')) {
+    loadModal();
+  }
 })
