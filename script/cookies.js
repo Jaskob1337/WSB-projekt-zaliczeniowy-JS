da7d54556c1988435b1bc653807f609288fe7cfd
Janek <you@example.comjaskobix@gmail.com>
2022-05-20 22:14:12 +0200

    fixed form sass

--- script/cookies.js
+++ script/cookies.js
@@ -1,5 +1,5 @@
 
-import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.mjs'
+import Cookies from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.mjs' 
 const loadModal = function () {
   $('#CarModal').modal('show')
   Cookies.set('isMyModalActive', true)
--- script/js.js
+++ script/js.js
@@ -76,7 +76,7 @@ const checked2 = JSON.parse(localStorage.getItem("CaschId"));
 
 // date car choose
 
-const dateChoose = document.getElementById('car-date-picker')
+const dateChoose = document.getElementById('car-date-picker') 
 
 dateChoose.min = getDate()
 dateChoose.max = getDate(14)
